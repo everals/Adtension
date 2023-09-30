d1c38229914311344c9d4c87b429df22c3c7864a
Everal <89090080110@mail.ru>
2023-09-30 15:14:54 +0500

    banners

---
+++ Adtension/Controllers/BannerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+using Adtension.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Adtension.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class BannerController : ControllerBase
+{
+    [HttpGet("{banner-id}")]
+    public async Task<BannerResponse> Get([FromRoute(Name = "banner-id")] Guid bannerId)
+    {
+        return new BannerResponse()
+        {
+            Id = Guid.NewGuid(),
+            Position = new Point(666, 666),
+            Size = new Size(100, 100)
+        };
+    }
+}
---
+++ Adtension/Models/BannerResponse.cs
@@ -0,0 +1,15 @@
+﻿using System.Drawing;
+
+namespace Adtension.Models;
+
+public class BannerResponse
+{
+    public Guid Id { get; set; }
+    public string Text { get; set; }
+    public string ImageLink { get; set; }
+    public string Color { get; set; }
+    public string ContentLink { get; set; }
+    public Point Position { get; set; }
+    public Size Size { get; set; }
+    public Guid EffectId { get; set; }
+}
