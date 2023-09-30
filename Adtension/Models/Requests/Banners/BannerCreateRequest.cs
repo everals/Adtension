43bd1c2ee116f6994e7105d0f98cde2e5a1083c2
Runoi <75607022+Runoi@users.noreply.github.com>
2023-09-30 14:53:56 +0300

    Request
    Request

--- Adtension/Models/Requests/Banners/BannerCreateRequest.cs
+++ Adtension/Models/Requests/Banners/BannerCreateRequest.cs
@@ -2,5 +2,12 @@
 
 public class BannerCreateRequest
 {
-    
+   
+    public string Text { get; set; }
+    public string ImageLink { get; set; }
+    public string Color { get; set; }
+    public string ContentLink { get; set; }
+    public Point? Position { get; set; }
+    public Size? Size { get; set; }
+    public Guid? EffectId { get; set; }
 }
--- Adtension/Models/Requests/Banners/BannerPatchRequest.cs
+++ Adtension/Models/Requests/Banners/BannerPatchRequest.cs
@@ -6,4 +6,11 @@ public class BannerPatchRequest
 {
     [JsonIgnore]
     public Guid Id { get; set; }
+    public string Text { get; set; }
+    public string ImageLink { get; set; }
+    public string Color { get; set; }
+    public string ContentLink { get; set; }
+    public Point? Position { get; set; }
+    public Size? Size { get; set; }
+    public Guid? EffectId { get; set; }
 }
